a52c367b4b299a7fce659940dc955aaf76869231
Alexander Bock <mail@alexanderbock.eu>
2026-05-15 20:43:10 +0200

    Split into separate chunks

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,23 @@ import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react(), nodePolyfills()]
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['events', 'buffer', 'stream']
+    })
+  ],
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            { name: 'react-vendor', test: /node_modules[\\/]react/, priority: 20 },
+            { name: 'mantine', test: /node_modules[\\/]@mantine/, priority: 15 },
+            { name: 'vendor', test: /node_modules/, priority: 10 }
+          ]
+        }
+      }
+    }
+  }
 });
